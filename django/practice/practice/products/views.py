18d83ad9f78015f3d70594e5d4c609dc4543cc86
sh <nshh9069@gmail.com>
2023-04-26 16:14:02 +0900

    3주차

---
+++ django/practice/practice/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializer import ProductSerializer
+from .models import Product
+
+# Create your views here.
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
